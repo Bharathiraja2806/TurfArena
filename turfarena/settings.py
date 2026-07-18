9d5d926a279f65d63db10a7604f14c4147f30f8c
Bharathiraja2806 <bharathiraja9708@gmail.com>
2026-07-18 09:05:20 +0530

    Third commit

--- turfarena/settings.py
+++ turfarena/settings.py
@@ -12,6 +12,13 @@ CSRF_TRUSTED_ORIGINS = [
     "https://turfarena-hhvr.onrender.com",
 ]
 
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 INSTALLED_APPS = [
     "django.contrib.admin", "django.contrib.auth", "django.contrib.contenttypes",
     "django.contrib.sessions", "django.contrib.messages", "django.contrib.staticfiles",
